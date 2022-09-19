e7e9299a5441454ecdb81f3ba697d4e98bf4c416
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2022-09-19 08:06:09 +0530

    This program is to find target element at matrix.

--- app/src/main/java/com/example/datastructurecompetive/TwoDimArr/PrintRowColWIseElement.java
+++ app/src/main/java/com/example/datastructurecompetive/TwoDimArr/PrintRowColWIseElement.java
@@ -4,6 +4,9 @@ package com.example.datastructurecompetive.TwoDimArr;
 //output
 //1 2 3 4 5 6 7 8 9 i am going to print the column major below
 //1 4 7 2 5 8 3 6 9.
+//this program is to find target element at matrix.
+//output
+//element found
 public class PrintRowColWIseElement {
     public static void main(String[] args) {
         int max = 3;
@@ -11,6 +14,8 @@ public class PrintRowColWIseElement {
         rowMajor(arr,max);
         System.out.println("i am going to print the column major below");
         columnMajor(arr,max);
+        searchElement(arr,max,2);
+
     }
 
     public static void rowMajor(int[][] arr, int max){
@@ -27,6 +32,19 @@ public class PrintRowColWIseElement {
                 System.out.print(arr[j][i]+" ");
             }
         }
+    }
+
+    public static boolean searchElement(int[][] arr, int max,int target){
+        for(int i = 0; i<max; i++){
+            for(int j= 0 ; j< max;j++){
+                if(arr[i][j] == target){
+                    System.out.println("element found: ");
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 
