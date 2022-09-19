d189fc2f582156fa90bd746753ced788c966c8aa
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2022-09-19 21:07:51 +0530

    // This program is also to print largest row sum.

--- app/src/main/java/com/example/datastructurecompetive/TwoDimArr/RowWiseSum.java
+++ app/src/main/java/com/example/datastructurecompetive/TwoDimArr/RowWiseSum.java
@@ -1,6 +1,14 @@
 package com.example.datastructurecompetive.TwoDimArr;
 
 // This program is to print row wise sum of 2d array
+//output
+// row sum is : 6
+//row sum is : 15
+//row sum is : 24
+
+// This program is also to print largest row sum.
+// largest row sum is 24.
+
 public class RowWiseSum {
     public static void main(String[] args) {
         int max = 3;
@@ -9,14 +17,21 @@ public class RowWiseSum {
         findRowWiseSum(arr,max,sum);
     }
 
-    public static void findRowWiseSum(int[][] arr, int max, int sum){
-        //traversing rowwise to find sum of each row
+    public static int findRowWiseSum(int[][] arr, int max, int sum){
+        //traversing row-wise to find sum of each row
+        int largestRowSum = 0;
         for(int i = 0 ; i < max; i++){
             for(int j = 0 ; j < max; j++){
                 sum = sum + arr[i][j];
             }
             System.out.println("row sum is : "+sum);
+            if(sum >= largestRowSum) {
+                largestRowSum = sum;
+            }
             sum = 0;
         }
+        System.out.println("largest row sum is: "+largestRowSum);
+        return largestRowSum;
+
     }
 }
