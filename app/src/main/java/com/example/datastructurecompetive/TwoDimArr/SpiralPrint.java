a1550ad22f492b19c8beb6cc26a0a6bcc14dc908
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2022-09-20 21:50:36 +0530

    This program is to print spiral in 2D array.Printed first row and last column of the array.//first row , last column , last row . //out  1 2 3 6 9 8 7    -

--- app/src/main/java/com/example/datastructurecompetive/TwoDimArr/SpiralPrint.java
+++ app/src/main/java/com/example/datastructurecompetive/TwoDimArr/SpiralPrint.java
@@ -2,7 +2,8 @@ package com.example.datastructurecompetive.TwoDimArr;
 
 //This program is to print spiral in 2D array
 //printed first row and last column of the array.
-//out
+//first row , last column , last row .
+//out  1 2 3 6 9 8 7    -
 public class SpiralPrint {
     public static void main(String[] args) {
         int max = 3;
@@ -17,11 +18,22 @@ public class SpiralPrint {
                      if(i == 0){
                          System.out.print(arr[i][j] + " ");
                      }
+                     if((i == n) && (j == n-1 )){
+                         for(int k = n-1; k>= 0; k--){
+                             System.out.print(arr[i][k] + " ");
+                         }
+                     }
                      if((i == 1) && (j == n)){
                          for(int k = 1; k< max ;k++){
                              System.out.print(arr[k][n] + " ");
                          }
                      }
+
+//                     if((i == 1) && (j == 0 )){
+//                         for(int k = 0; k<n; k++){
+//                             System.out.print(arr[i][k] + " ");
+//                         }
+//                     }
 
                  }
 
